e7c4b4b210a302655794090538254bc1fd49bd67
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-04-08 04:00:45 -0300

    Improved how percentage is displayed

--- populateDB_part1.sh
+++ populateDB_part1.sh
@@ -4,20 +4,19 @@
 
 total=`ls -1 2021-03-25/|wc -l`
 iter=1
-clear
 for file in `ls -1 -t -r 2021-03-25/`
 do
+	clear
 	echo "Populating the database."
 	echo "Please do not turn off the laptop until the operation has finished"
 	
 	echo ""
 	echo ""
 
-	./insertTweetsAndUsersToDB.pl "2021-03-25/$file"
 	echo "$file log-> $?" >> log.txt
 	perc=`bc <<< "$iter * 100 / $total"`
 	echo "Percentage of scanned files: $perc"
+	./insertTweetsAndUsersToDB.pl "2021-03-25/$file"
 	iter=$((iter+1))
-	clear
 done
 
