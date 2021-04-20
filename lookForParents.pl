50166c9b2dc928ccd3179190707ba58b71b272d5
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-04-20 17:15:33 -0300

    Set the skeleton of the new perl script

--- lookForParents.pl
+++ lookForParents.pl
@@ -1,21 +1,40 @@
-#/usr/bin/perl
+#!/usr/bin/perl
 # vim: foldmethod=marker: set tw=80:
 
-#|--- Documentation {{{
+#|--- Documentation {{{1
 # 
 # GOAL: we will look for all tweet parents
 # of each tweet stored into the twitter.db database.
-#}}}
+# 1}}}
 
-#|--- Modules {{{
+#|--- Modules {{{1
 use warnings;
 use strict;
-# }}}
+# 1}}}
 
-#|--- Lets find out the tweet_ids from twitter.db that we need to look up {{{
+#|--- Auxiliary Functions {{{1
+# END OF AUXILIARY FUNCTIONS 1}}}
+
+#|--- MAIN Program {{{1
+#|--- Variables {{{2
+my @tweet_ids;
+
+my $i;
+# 2}}}
+
+#|--- Loop untill we have found all parents of each tweet that we have {{{2
+while(1){
+
+#|--- Lets find out the tweet_ids from twitter.db that we need to look up {{{3
 
 # We will store these tweet_ids into an array
-#}}}
+# 3}}}
+	last if (!defined(@check));
 
-#|--- Given the tweet_ids above lets find out their parents {{{
-#}}}
+#|--- Given the tweet_ids above lets find out their parents {{{3
+
+# 3}}}
+} 
+# END OF THE LOOP 2}}}
+# END OF MAIN 1}}}
+
