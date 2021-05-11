b0751b9b9b39f445ed3c8d4ca3baf0fb4883a46d
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-05-11 20:48:03 -0300

    paths.sql added

---
+++ graph_tools/paths.sql
@@ -0,0 +1,15 @@
+CREATE TABLE paths_xy (
+	x 	     INT NOT NULL,
+	y 	     INT NOT NULL,
+	generation_x INT NOT NULL,
+	generation_y INT NOT NULL
+);
+
+CREATE TABLE paths_xyz (
+	x 	     INT NOT NULL,
+	y 	     INT NOT NULL,
+	z	     INT NOT NULL, 
+	generation_x INT NOT NULL,
+	generation_y INT NOT NULL,
+	generation_z INT NOT NULL
+);
