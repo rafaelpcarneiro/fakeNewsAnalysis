aa0c7b5de5dcc331b5e6052129868e8dd73674ac
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-05-14 17:08:19 -0300

    Renamed some files to better names

--- graph_tools/get_db.sh
+++ graph_tools/get_db.sh
@@ -3,5 +3,5 @@ cp ~/twitter.db -t ./
 #sqlite3 twitter.db ".read root_view.sql"
 #perl nodes_tree.pl
 
-sqlite3 twitter.db ".read paths.sql"
-perl paths_xy.pl
+sqlite3 twitter.db ".read edges.sql"
+perl edges_xy.pl
