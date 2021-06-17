0f908f244e892ae929b1b8aeeaee27c1745f067e
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-06-17 18:16:50 -0300

    added sampling function for the dictionary

---
+++ classifying_fake_news/sampling.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+# vim: set ts=4 expandtab sw=4 foldmethod=marker:
+
+
+#|--- Documentation {{{1
+# 1}}}
+
+#|--- Modules {{{1
+use warnings;
+use strict;
+use DBI;
+#use open ':encoding(utf8)';
+binmode(STDOUT, ":utf-8");
+
+use POSIX;
+#1}}}
+
+#|--- MAIN {{{1
+
+
+# Relational Database
+my $dbfile   = 'twitter.db';
+my $dsn      = "dbi:SQLite:dbname=$dbfile";
+my $user     = '';
+my $password = '';
+my $dbh      = DBI->connect ($dsn, $user, $password, {
+	PrintError		 => 0,
+	RaiseError		 => 1,
+	AutoCommit		 => 1,
+	FetchHashKeyName => 'NAME_lc',
+});
+
+my $sql_query01;
+
+my $text;
+my $tweet_id;
+
+my $counter;
+
+my @tweetID_text;
+
+
+$sql_query01 =  $dbh->prepare ("SELECT tweet_id, text
+			                    FROM tweet
+                                WHERE tweet_id IN (SELECT x.tweet_id 
+                                                   FROM nodes AS x
+                                                   WHERE tweet_id = x.tweet_id 
+                                                         AND
+                                                         x.generation_of_tweet_id = 0)
+                                      AND created_at IS NOT NULL
+                                      AND text IS NOT NULL");
+
+
+$sql_query01->execute ();
+$counter = 0;
+while (($tweet_id, $text) = $sql_query01->fetchrow_array) {
+    push @tweetID_text, {tweet_id => $tweet_id, text => $text};
+    
+    print "Reading  ==> tweet_id = $tweet_id \n";
+    ++$counter;
+}
+# Counter = 7897
+
+# Sampling
+my $sample;
+my $fh;
+my $dict;
+
+open $fh, '>', 'naiveBayes.sample'
+    or die "Problems to create the sample file";
+
+srand (666);
+foreach (1..100) {
+    $sample = floor (rand ($counter));
+
+    $dict = $tweetID_text [$sample];
+    print $fh "TWEET_ID == $dict->{'tweet_id'}\n"; 
+    print $fh "TEXT\n$dict->{'text'}\n\n"; 
+}
+close $fh;
+
+
+
+
+# $dbh->disconnect;
+
+
+# END MAIN 1}}} 
