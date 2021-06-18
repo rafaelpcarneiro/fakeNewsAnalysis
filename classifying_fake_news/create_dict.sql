a79495d61495a82c1a861ee95075fd9113a72aaf
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-06-18 17:35:23 -0300

    renamed last entry from create_dict.sql

--- classifying_fake_news/create_dict.sql
+++ classifying_fake_news/create_dict.sql
@@ -1,10 +1,10 @@
 -- vim: set ts=4 expandtab sw=4:
 CREATE TABLE dictionary (
-	tweet_id				            INT,
-	word_found_on_tweet_id		        TEXT NOT NULL,
-	word_counter_considering_tweet_id	INT  NOT NULL,
-	was_this_tweet_id_sampled		    INT  DEFAULT 0,
-	if_tweet_id_was_sampled_is_it_fake	INT  DEFAULT 0,
+	tweet_id				                    INT,
+	word_found_on_tweet_id		                TEXT NOT NULL,
+	word_counter_considering_tweet_id	        INT  NOT NULL,
+	was_this_tweet_id_sampled		            INT  DEFAULT 0, -- yes = 1, no = 0
+	if_tweet_id_was_sampled_is_it_unreliable	INT  DEFAULT 0, -- yes = 1, no = 0
 
 	PRIMARY KEY (tweet_id, word_found_on_tweet_id)
 );
