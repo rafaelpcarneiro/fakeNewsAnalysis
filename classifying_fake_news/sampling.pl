7b362db6ae8157bab36377391188b4d97fa7a4b6
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-06-19 16:58:01 -0300

    Taking a larger sample

--- classifying_fake_news/sampling.pl
+++ classifying_fake_news/sampling.pl
@@ -43,16 +43,21 @@ my $counter;
 
 my @tweetID_text;
 
-
-$sql_query01 =  $dbh->prepare ("SELECT tweet_id, text
-			                    FROM tweet
-                                WHERE tweet_id IN (SELECT x.tweet_id 
-                                                   FROM nodes AS x
-                                                   WHERE tweet_id = x.tweet_id 
-                                                         AND
-                                                         x.generation_of_tweet_id = 0)
-                                      AND created_at IS NOT NULL
-                                      AND text IS NOT NULL");
+$sql_query01 =  $dbh->prepare ("SELECT 
+                                    tweet_id, text
+			                    FROM
+                                    tweet
+                                WHERE
+                                    tweet_id IN (SELECT
+                                                    x.tweet_id 
+                                                 FROM
+                                                    nodes AS x
+                                                 WHERE
+                                                    tweet_id = x.tweet_id)
+                                     AND
+                                     created_at IS NOT NULL
+                                     AND
+                                     text IS NOT NULL");
 
 
 $sql_query01->execute ();
@@ -60,26 +65,46 @@ $counter = 0;
 while (($tweet_id, $text) = $sql_query01->fetchrow_array) {
     push @tweetID_text, {tweet_id => $tweet_id, text => $text};
     
-    print "Reading  ==> tweet_id = $tweet_id \n";
+    #print "Reading  ==> tweet_id = $tweet_id \n";
     ++$counter;
 }
-# Counter = 7897
 
 # Sampling
 my $sample;
 my $fh;
 my $dict;
+my $counter2;
+my @rand_numbers;
+my $j;
 
 open $fh, '>', 'naiveBayes.sample'
     or die "Problems to create the sample file";
 
 srand (666);
-foreach (1..700) {
-    $sample = floor (rand ($counter));
+$sample   = floor (rand ($counter));
+push        @rand_numbers, $sample;
+$counter2 = @rand_numbers;
+
+while ($counter2 < 4072) {
+    print "Sample $counter2\n";
 
     $dict = $tweetID_text [$sample];
     print $fh "TWEET_ID == $dict->{'tweet_id'}\n"; 
     print $fh "TEXT\n$dict->{'text'}\n\n"; 
+
+    $sample = floor (rand ($counter));
+    $j      = 0;
+    while (j < $counter2) {
+        if ($rand_numbers [$j] == $sample) {
+            $j = 0;
+            $sample = floor (rand ($counter));
+        }
+        else {
+            ++$j;
+        }
+    }
+    push        @rand_numbers, $sample;
+    ++$counter2;
 }
 close $fh;
 
