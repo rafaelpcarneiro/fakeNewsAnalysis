ff820829bc930a679ab5b74b93a1f73ad62066b1
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-06-18 23:36:36 -0300

    rename all probability entries on create_dict.sql

--- classifying_fake_news/create_dict.sql
+++ classifying_fake_news/create_dict.sql
@@ -9,7 +9,7 @@ CREATE TABLE dictionary (
 	PRIMARY KEY (tweet_id, word_found_on_tweet_id)
 );
 
--- Down below I take the follow names X Theta in 
+-- Down below I use the following names, X and Theta, in 
 -- order to express random variables. They mean:
 -- X is our info from the universe of all tweets
 -- Theta = 1 or 0 meaning that the tweet is unreliable (Theta = 1)
@@ -17,22 +17,20 @@ CREATE TABLE dictionary (
 CREATE TABLE naiveBayes (
     tweet_id                 INT,
     is_tweet_id_unreliable   INT  DEFAULT 0, -- yes = 1, no = 0
-    p[Theta=1|X]             REAL DEFAULT 0.0,
-    p[Theta=0|X]             REAL DEFAULT 0.0,
+    prob_Theta_eq_1_given_X  REAL DEFAULT 0.0,
+    prob_Theta_eq_0_given_X  REAL DEFAULT 0.0,
 
     PRIMARY KEY (tweet_id)
 );
 
--- Down below I take the follow names X Theta in 
--- order to express random variables. They mean:
--- W is the multinomial random variable whose values range
---   between all words from our sample
+-- Down below I use the name  Theta
+-- order to express a random variable. It means:
 -- Theta = 1 or 0 meaning that the tweet is unreliable (Theta = 1)
 --   or not, otherwise (Theta = 0).
 CREATE TABLE probabilities (
-    word_sampled        TEXT,
-    p[W=word|Theta=1]   REAL DEFAULT 0.0,
-    p[W=word|Theta=0]   REAL DEFAULT 0.0,
+    word_sampled                      TEXT,
+    prob_find_word_given_Theta_eq_1   REAL DEFAULT 0.0,
+    prob_find_word_given_Theta_eq_0   REAL DEFAULT 0.0,
 
     PRIMARY KEY (word_sampled)
 );
