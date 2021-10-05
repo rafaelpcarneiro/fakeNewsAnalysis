14d6870abb1a52ce2eea71daf8248099837ab85f
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-10-05 18:21:36 -0300

    plot_graph_fakeNews/plot.sql

---
+++ plot_graph_fakeNews/plot.sql
@@ -0,0 +1,15 @@
+.open twitter.db
+
+.once nodes.txt
+SELECT DISTINCT author_tweet_id 
+FROM   tweet
+WHERE  tweet_id IN (SELECT x.tweet_id
+                    FROM nodes);
+
+separator "--"
+.once edges.txt
+SELECT a.author_tweet_id, b.author_tweet_id
+FROM tweet AS a, tweet AS b
+WHERE a.tweet_id IN (SELECT from_node FROM paths_xy)
+      AND
+      b.tweet_id IN (SELECT to_node FROM paths_xy)
