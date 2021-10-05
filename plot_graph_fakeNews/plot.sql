521cb0218170996bd730287128b6848f305aa888
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-10-05 18:41:21 -0300

    fixed .separator and another error

--- plot_graph_fakeNews/plot.sql
+++ plot_graph_fakeNews/plot.sql
@@ -3,13 +3,13 @@
 .once nodes.txt
 SELECT DISTINCT author_tweet_id 
 FROM   tweet
-WHERE  tweet_id IN (SELECT x.tweet_id
+WHERE  tweet_id IN (SELECT nodes.tweet_id
                     FROM nodes);
 
--- separator "--"
--- .once edges.txt
--- SELECT a.author_tweet_id, b.author_tweet_id
--- FROM tweet AS a, tweet AS b
--- WHERE a.tweet_id IN (SELECT from_node FROM paths_xy)
---       AND
---       b.tweet_id IN (SELECT to_node FROM paths_xy)
+.separator "--"
+.once edges.txt
+SELECT a.author_tweet_id, b.author_tweet_id
+FROM tweet AS a, tweet AS b
+WHERE a.tweet_id IN (SELECT from_node FROM paths_xy)
+      AND
+      b.tweet_id IN (SELECT to_node FROM paths_xy)
