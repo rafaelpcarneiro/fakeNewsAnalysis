24b78b149c7f0c488f3a0934dfcd90718cdf936f
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-10-05 22:35:10 -0300

    paths_xy will have the users ids

--- graph_tools/edges.pl
+++ graph_tools/edges.pl
@@ -39,12 +39,16 @@ my $sql_query01;
 
 my $sql_query02;
 
+my $sql_query03;
+
 my $sql_insert;
 
 my $generation;
 my $iterator;
-my $from_generation;
-my $to_generation;
+
+my $from_tweet_id, my $to_tweet_id;
+
+my $from_author_tweet_id, my $to_author_tweet_id;
 
 # END VARIABLES 2}}}
 
@@ -52,26 +56,51 @@ my $to_generation;
 #|--- Generating regular paths [x,y] of dimension 1  {{{2
 
 #|--- Declaring SQL commands to be executed at each generation {{{3
-$sql_query01 =  $dbh->prepare ("SELECT parent_tweet_id, tweet_id
+$sql_query01 =  $dbh->prepare (
+                   "SELECT parent_tweet_id, tweet_id
 			        FROM tweet
 			        WHERE tweet_id IN (SELECT X.tweet_id
 					  	   FROM   nodes AS X
-						   WHERE  X.generation_of_tweet_id = ?)");
+						   WHERE  X.generation_of_tweet_id = ?)"
+                );
 
-$sql_query02 = $dbh->prepare ("SELECT MAX(amount_of_sons)
+$sql_query02 = $dbh->prepare (
+                  "SELECT MAX(amount_of_sons)
 			       FROM nodes
-			       WHERE generation_of_tweet_id = ?");
+			       WHERE generation_of_tweet_id = ?"
+               );
 
-$sql_insert  = $dbh->prepare ("INSERT INTO paths_xy VALUES (?,?,?,?)");
+$sql_query03 = $dbh->prepare (
+                  "SELECT author_tweet_id
+			       FROM   tweet
+			       WHERE  tweet_id = ?"
+               );
+
+$sql_insert  = $dbh->prepare ("INSERT INTO paths_xy VALUES (?,?,?,?,?,?)");
 # 3}}}
 
 #|--- Executing SQL commands at each generation {{{3
 $generation = 1;
 do {
 	$sql_query01->execute ($generation);
-	while (($from_generation, $to_generation) = $sql_query01->fetchrow_array) {
-		$sql_insert->execute ($from_generation, $to_generation, $generation - 1, $generation);
-		print "[$from_generation, $to_generation]", "   ",  $generation - 1, "  ", $generation, "\n";
+	while (($from_tweet_id, $to_tweet_id) = $sql_query01->fetchrow_array) {
+
+        $sql_query03->execute ($from_tweet_id);
+        ($from_author_tweet_id) = $sql_query03->fetchrow_array;
+
+        $sql_query03->execute ($to_tweet_id);
+        ($to_author_tweet_id) = $sql_query03->fetchrow_array;
+
+		$sql_insert->execute ($from_tweet_id,
+                              $to_tweet_id,
+                              $from_author_tweet_id,
+                              $to_author_tweet_id,
+                              $generation - 1,
+                              $generation);
+
+		print "[$from_tweet_id, $to_tweet_id]",               "    ",
+		      "[$from_author_tweet_id, $to_author_tweet_id]", "----",
+              $generation - 1, "  ", $generation, "\n";
 	}
 
 	$sql_query02->execute ($generation);
--- graph_tools/edges.sql
+++ graph_tools/edges.sql
@@ -1,6 +1,8 @@
 CREATE TABLE paths_xy (
-	from_node      	 INT NOT NULL,
-	to_node	       	 INT NOT NULL,
-	generation_from	 INT NOT NULL,
-	generation_to	 INT NOT NULL
+	from_tweet_id      	 INT NOT NULL,
+	to_tweet_id	       	 INT NOT NULL,
+	from_author_tweet_id INT NOT NULL,
+	to_author_tweet_id	 INT NOT NULL,
+	generation_from	     INT NOT NULL,
+	generation_to	     INT NOT NULL
 );
