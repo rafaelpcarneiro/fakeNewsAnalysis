93b05451c30ca00771319935871f12f7fd79da50
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-11-17 00:39:48 -0300

    sample size = 40%

--- filtration_v02/take_a_sample.sql
+++ filtration_v02/take_a_sample.sql
@@ -37,7 +37,7 @@ CREATE TABLE paths_xy_SAMPLE (
     PRIMARY KEY (from_tweet_id, to_tweet_id)
 );
 
--- Take a sample size equal to 30% of the numberOfEdges
+-- Take a sample size equal to 40% of the numberOfEdges
 INSERT INTO aSample
 SELECT DISTINCT
     from_author_tweet_id,
@@ -51,7 +51,7 @@ WHERE
     AND
     path_distance = 1
 ORDER BY random()
-LIMIT CAST(.30 * (SELECT * FROM numberOfEdges) AS INT);
+LIMIT CAST(.40 * (SELECT * FROM numberOfEdges) AS INT);
 
 -- Fill paths_xy_SAMPLE with the sample
 INSERT INTO paths_xy_SAMPLE 
