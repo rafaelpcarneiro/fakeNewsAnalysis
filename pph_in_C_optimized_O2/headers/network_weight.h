b6d97a77006d96946fd8f851678ec0ac6d29606c
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-11-20 01:44:21 -0300

    optimized 2

---
+++ pph_in_C_optimized_O2/headers/network_weight.h
@@ -0,0 +1,31 @@
+/* vim: set ts=4 expandtab sw=4: */
+/*================================================================
+ * DOCUMENTATION> please read the file doc.pdf (inside the folder
+ * pph_in_C), it explains everething that you will find here.
+ *
+ * ---------------------------------------------------------------
+ *================================================================*/
+
+#ifndef __NETWORK_WEIGHT_H_
+#define __NETWORK_WEIGHT_H_
+
+#include "definitions.h"
+
+#define FILE_EDGES "data/edges.txt"
+
+typedef struct {
+    vertex_index from;
+    vertex_index to;
+    double       weightEdge;
+} graphWeight;
+
+typedef struct {
+    graphWeight  *thegraphWeight;
+    unsigned int size;
+} graphWeightList;
+
+graphWeightList *alloc_graphWeightMatrix (void);
+
+double          network_weight (vertex_index, vertex_index, graphWeightList*);
+
+#endif /* __NETWORK_WEIGHT_H_ */
---
+++ pph_in_C_optimized_O2/src/network_weight.c
@@ -0,0 +1,64 @@
+/* vim: set ts=4 expandtab sw=4: */
+
+/*================================================================
+ * DOCUMENTATION> please read the file doc.pdf (inside the folder
+ * pph_in_C), it explains everething that you will find here.
+ *
+ * ---------------------------------------------------------------
+ *================================================================*/
+
+#include "../headers/network_weight.h"
+#include "../headers/definitions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+graphWeightList *alloc_graphWeightMatrix (void) {
+    FILE         *fh;
+    unsigned int i;
+    vertex_index a, b;
+    unsigned int weight_matrix_size;
+    double       weight_tmp;
+
+    graphWeightList  *weight  = malloc (sizeof (graphWeightList));
+
+
+    fh = fopen (FILE_EDGES, "r");
+
+    if (fh == NULL) 
+        printf ("Problems reading the file with the matix of the weights\n\n");
+
+    fscanf (fh, "%u", &weight_matrix_size);
+
+    weight->thegraphWeight = malloc (weight_matrix_size * sizeof(graphWeight));
+    weight->size           = weight_matrix_size;
+
+    i = 0;
+    while ( fscanf (fh, "%u %u %lf", &a, &b, &weight_tmp) != EOF ) {
+        (weight->thegraphWeight + i)->from       = a;
+        (weight->thegraphWeight + i)->to         = b;
+        (weight->thegraphWeight + i)->weightEdge = weight_tmp;
+        ++i;
+    }
+    fclose (fh);
+
+    return weight;
+}
+
+double network_weight (vertex_index x, vertex_index y, graphWeightList *W) {
+
+    unsigned int    i;
+    double          weight_to_return = 0.0; 
+    /* unsigned int test; */
+
+    for (i = 0; i < W->size; ++i) {
+        if ( ((W->thegraphWeight + i)->from == x) && ((W->thegraphWeight + i)->to == y)) {
+         
+            weight_to_return = (W->thegraphWeight + i)->weightEdge;
+            break;
+        }
+    }
+
+    /* printf ("netw = %u\n", test); */ 
+    /* return (double) (test); */
+    return weight_to_return;
+}
