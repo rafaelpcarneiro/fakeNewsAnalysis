d923f1f17052bdd0378171439e88ba668c49f901
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2021-11-27 02:06:48 -0300

    new generate_nodes_and_edges for branches.c on filtration

--- filtration/generate_nodes_and_edges_to_txt.sql
+++ filtration/generate_nodes_and_edges_to_txt.sql
@@ -1,11 +1,33 @@
 .separator "    "
 .output nodes.txt
-SELECT COUNT(*) FROM nodes;
-SELECT * FROM nodes;
+SELECT COUNT(*) 
+FROM nodes
+WHERE
+    tweet_id IN (SELECT from_tweet_id 
+                 FROM   paths_xy 
+                 WHERE  from_author_tweet_id != -1 AND path_distance = 1
+    	         UNION
+    	         SELECT to_tweet_id FROM paths_xy WHERE  path_distance = 1);
+SELECT *
+FROM nodes
+WHERE
+    tweet_id IN (SELECT from_tweet_id 
+                 FROM   paths_xy 
+                 WHERE  from_author_tweet_id != -1 AND path_distance = 1
+    	         UNION
+    	         SELECT to_tweet_id 
+                 FROM   paths_xy 
+                 WHERE  path_distance = 1)
+ORDER BY generation_of_tweet_id ASC;
 
 .output edges.txt
-SELECT COUNT(*) FROM paths_xy;
-SELECT * FROM paths_xy;
-
+SELECT COUNT(*)
+FROM paths_xy
+WHERE
+    from_author_tweet_id != -1 AND path_distance = 1;
+SELECT from_tweet_id, to_tweet_id, 1
+FROM paths_xy
+WHERE
+    from_author_tweet_id != -1 AND path_distance = 1;
 
 .exit
