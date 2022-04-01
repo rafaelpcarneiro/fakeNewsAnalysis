1eb7d484fd5ad6d4604d98dc6bedce4018d14b44
rafaelpcarneiro <rafaelpcarneiro@gmail.com>
2022-03-31 23:42:58 -0300

    minor mistakes

--- analysis/calculate_pph_from_samples.sh
+++ analysis/calculate_pph_from_samples.sh
@@ -1,24 +1,24 @@
 # cpu_core = 0, 1, 2, 3
 cpu_core=$1
 loopStart=$((cpu_core * 300))
-loopEnd=$((loopstart + 300))
+loopEnd=$((loopStart + 300))
 
 cd ../pph_in_C
 make
 make clean
-mv pph_prog -t "../analysis/filtration_samples$(cpu_core)"
+mv pph_prog -t ../analysis/filtration_samples$cpu_core
 cd ../analysis/
 
 for graph in mariliaMendonca football f1 mitoVisitaPutin onu politcsAR
 do
-    cp $graph/twitter.db -t "filtration_samples$(cpu_core)/" 
+    cp $graph/twitter.db -t filtration_samples$cpu_core/ 
 
     cd $graph/
     dateStart=`head -n 1 dates.txt`
     dateEnd=`tail -n 1 dates.txt`
     sampleSize=20
 
-    cd "../filtration_samples$(cpu_core)/"
+    cd ../filtration_samples$cpu_core/
 
     #for i in {0..299};
     i=$loopStart
